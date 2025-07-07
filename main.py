c3f36dd3a22593aa58b00a3c18441334b5cb2e50
ffsalomon33 <ffsalomon@gmail.com>
2025-07-07 09:31:54 -0500

    Created Game Loop with simple Draw

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-import pygame, constants
+import pygame
+from constants import *
+
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False 
+                 
+        screen.fill((0, 0, 0))
+
+        pygame.display.flip()
+
 
 
 if __name__ == "__main__":
